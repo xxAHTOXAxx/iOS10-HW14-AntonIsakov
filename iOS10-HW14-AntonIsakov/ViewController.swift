21c01f81e8c7eb7a5814e264941acd415114d366
Anton <xxahtoxaxx2015@gmail.com>
2023-07-04 21:57:43 +0400

    Initial Commit

---
+++ iOS10-HW14-AntonIsakov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS10-HW14-AntonIsakov
+//
+//  Created by Антон Исаков on 04.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
